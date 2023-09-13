e142d39fa6db02fbdb0487691404eb2a11c13b5f
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-09-13 15:12:40 +0900

    워밍업7-3

--- warmingup7/warmingup7/Miro.cpp
+++ warmingup7/warmingup7/Miro.cpp
@@ -15,13 +15,55 @@ void Miro::showboard() {
 	}
 }
 void Miro::enter() {
-	int ran;
-	for (int i = 0; i < 50; i++) {
-		for (int j = 0; j < 50; j++) {
-			if (i == j) {
-				board[i][j].check = true;
+	int x=0, y=0;
+	int check = 0;
+	int ran3 = rand() % 20 + 10;
+	while (1) {
+		if(x<=49||y<=49) {
+			int ran = (rand() % 4);
+			for (int i = 0; i < ran; i++) {
+				if (x >= 49&&y!=49) {
+					for (int i = 1; i < ran; i++) {
+						x--;
+						board[y][x].check = true;
+					}
+				}
+				if (y >= 49&&x!=49) {
+					for (int i = 1; i < ran; i++) {
+						y--;
+						board[y][x].check = true;
+					}
+				}
+				int ran2 = rand() % 2;
+				switch (ran2) {
+				case 0:
+					if (check == 1) {
+						check = 0;
+						break;
+					}
+					else {
+						board[y][x].check = true;
+						x++;
+						check = 1;
+						break;
+					}
+				case 1:
+					if (check == 0) {
+						check = 1;
+						break;
+					}
+					else {
+						board[y][x].check = true;
+						y++;
+						check = 0;
+						break;
+					}
+				}
 			}
-			ran=rand() % 4 + 1;
+		}
+		else {
+			board[y][x].check = true;
+			break;
 		}
 	}
 }
