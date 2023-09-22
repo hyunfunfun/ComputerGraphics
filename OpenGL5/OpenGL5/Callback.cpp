9988deacb61716c50a6b752c2608deda25a9ca32
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-09-23 04:11:18 +0900

    OpenGL5-3(클릭시 지우기 -> 클릭시 지우개 생성)

--- OpenGL5/OpenGL5/Callback.cpp
+++ OpenGL5/OpenGL5/Callback.cpp
@@ -1,7 +1,7 @@
 #include "Header.h"
 #include "Callback.h"
 
-float squre[20][7];
+float squre[21][7];
 float rectsize = 0.05;
 float prevx;
 float prevy;
@@ -25,7 +25,7 @@ GLvoid defsqure() {
 GLvoid drawScene() {
 	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 21; i++) {
 		glColor3f(squre[i][4], squre[i][5], squre[i][6]);
 		glRectf(squre[i][0], squre[i][1], squre[i][2], squre[i][3]);
 	}
@@ -38,57 +38,67 @@ GLvoid Reshape(int w, int h) {
 GLvoid Mouse(int button, int state, int x, int y) {
 	float ogx = ((float)x / 500) - 1;
 	float ogy = -(((float)y / 500) - 1);
-	for (int i = 0; i < 20; i++) {
-		//클릭시 사각형 선택
-		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && ogx>squre[i][0] && ogx <squre[i][2] && ogy >squre[i][1] && ogy < squre[i][3]) {
+		//지우개 사각형 생성
+		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){	
+			squre[20][0] = ogx - (rectsize*2);
+			squre[20][1] = ogy - (rectsize * 2);
+			squre[20][2] = ogx + (rectsize * 2);
+			squre[20][3] = ogy + (rectsize * 2);
+			squre[20][4] = 0.0f;
+			squre[20][5] = 0.0f;
+			squre[20][6] = 0.0f;
+
 			prevx = ogx;
 			prevy = ogy;
 			button_down = true;
-			click = i;
 		}
 		//클릭버튼을 뗄 경우 사각형 삭제
-		else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP && ogx > squre[i][0] && ogx <squre[i][2] && ogy >squre[i][1] && ogy < squre[i][3]) {
-			squre[i][0] = NULL;
-			squre[i][1] = NULL;
-			squre[i][2] = NULL;
-			squre[i][3] = NULL;
+		else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP ){
+			squre[20][0] = NULL;
+			squre[20][1] = NULL;
+			squre[20][2] = NULL;
+			squre[20][3] = NULL;
 			button_down = false;
-		}
 	}
 }
 GLvoid Motion(int x, int y) {
 	float sizeup = 0.01;
 	float ogx = ((float)x / 500) - 1;
 	float ogy = -(((float)y / 500) - 1);
-	//클릭한 사각형 움직이기
+	//지우개 사각형 움직이기
 	if (button_down == true) {
-		squre[click][0] += (ogx - prevx);
-		squre[click][1] += (ogy - prevy);
-		squre[click][2] += (ogx - prevx);
-		squre[click][3] += (ogy - prevy);
+		squre[20][0] += (ogx - prevx);
+		squre[20][1] += (ogy - prevy);
+		squre[20][2] += (ogx - prevx);
+		squre[20][3] += (ogy - prevy);
 		prevx = ogx;
 		prevy = ogy;
 
-		//클릭한 사각형과 닿은 사각형지우고, 클릭한 사각형 크기증가
+		//지우개 사각형과 닿은 사각형지우고, 지우개 사각형 크기증가
 		for (int i = 0; i < 20; i++) {
-			if ( (squre[i][0] <= squre[click][2]) && (squre[i][1] <= squre[click][3]) && (squre[i][2] >= squre[click][0]) && (squre[i][3] >= squre[click][1])) {
-				if (i == click)
-					continue;
-				else {
-					squre[i][0] = NULL;
-					squre[i][1] = NULL;
-					squre[i][2] = NULL;
-					squre[i][3] = NULL;
-					squre[click][4] = squre[i][4];
-					squre[click][5] = squre[i][5];
-					squre[click][6] = squre[i][6];
-					squre[click][0] -= sizeup;
-					squre[click][1] -= sizeup;
-					squre[click][2] += sizeup;
-					squre[click][3] += sizeup;
-				}
+			if ( (squre[i][0] <= squre[20][2]) && (squre[i][1] <= squre[20][3]) && (squre[i][2] >= squre[20][0]) && (squre[i][3] >= squre[20][1])) {
+				squre[i][0] = -10;
+				squre[i][1] = -10;
+				squre[i][2] = -10;
+				squre[i][3] = -10;
+				squre[20][4] = squre[i][4];
+				squre[20][5] = squre[i][5];
+				squre[20][6] = squre[i][6];
+				squre[20][0] -= sizeup;
+				squre[20][1] -= sizeup;
+				squre[20][2] += sizeup;
+				squre[20][3] += sizeup;
 			}
 		}
 		glutPostRedisplay();
+	}
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'r':
+		defsqure();
+		glutPostRedisplay();
+		break;
 	}
 }
--- OpenGL5/OpenGL5/Callback.h
+++ OpenGL5/OpenGL5/Callback.h
@@ -6,3 +6,4 @@ GLvoid drawScene();
 GLvoid Reshape(int w, int h);
 GLvoid Mouse(int button, int state, int x, int y);
 GLvoid Motion(int x, int y);
+GLvoid Keyboard(unsigned char key, int x, int y);
