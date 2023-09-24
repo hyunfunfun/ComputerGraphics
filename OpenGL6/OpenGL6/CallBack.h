0b754bf29b3d23076699a2e3027d7db001af1fff
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-09-24 18:47:59 +0900

    OpenGL6-4 (5개 사각형 전부 분할)

--- OpenGL6/OpenGL6/CallBack.h
+++ OpenGL6/OpenGL6/CallBack.h
@@ -7,3 +7,6 @@ GLvoid Reshape(int w, int h);
 GLvoid Mouse(int button, int state, int x, int y);
 GLvoid Timer(int value);
 GLvoid Timer2(int value);
+GLvoid Timer3(int value);
+GLvoid Timer4(int value);
+GLvoid Timer5(int value);
--- OpenGL6/OpenGL6/Callback.cpp
+++ OpenGL6/OpenGL6/Callback.cpp
@@ -6,12 +6,15 @@ float divsqure1[4][7];
 float divsqure2[4][7];
 float divsqure3[4][7];
 float divsqure4[4][7];
-float divsqure5[4][7];
+float divsqure5[8][7];
 float rectsize = 0.1;
 int squreindx;
 
 bool sqr1stop = false;
 bool sqr2stop = false;
+bool sqr3stop = false;
+bool sqr4stop = false;
+bool sqr5stop = false;
 
 GLvoid defrect() {
 	for (int i = 0; i < 5; i++) {
@@ -41,6 +44,19 @@ GLvoid drawScene() {
 		glColor3f(divsqure2[i][4], divsqure2[i][5], divsqure2[i][6]);
 		glRectf(divsqure2[i][0], divsqure2[i][1], divsqure2[i][2], divsqure2[i][3]);
 	}
+	for (int i = 0; i < 4; i++) {
+		glColor3f(divsqure3[i][4], divsqure3[i][5], divsqure3[i][6]);
+		glRectf(divsqure3[i][0], divsqure3[i][1], divsqure3[i][2], divsqure3[i][3]);
+	}
+	for (int i = 0; i < 4; i++) {
+		glColor3f(divsqure4[i][4], divsqure4[i][5], divsqure4[i][6]);
+		glRectf(divsqure4[i][0], divsqure4[i][1], divsqure4[i][2], divsqure4[i][3]);
+	}
+	for (int i = 0; i < 8; i++) {
+		glColor3f(divsqure5[i][4], divsqure5[i][5], divsqure5[i][6]);
+		glRectf(divsqure5[i][0], divsqure5[i][1], divsqure5[i][2], divsqure5[i][3]);
+	}
+	
 
 	glutSwapBuffers();
 }
@@ -51,8 +67,8 @@ GLvoid Mouse(int button, int state, int x, int y) {
 	float ogx = ((float)x / 500) - 1;
 	float ogy = -(((float)y / 500) - 1);
 	//squre0 나누기
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && ogx > squre[0][0] && ogx <squre[0][2] && ogy >squre[0][1] && ogy < squre[0][3]) {
-		squreindx = 0;
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && 
+		ogx > squre[0][0] && ogx <squre[0][2] && ogy >squre[0][1] && ogy < squre[0][3]) {
 		for (int i = 0; i < 4; i++) {
 			switch (i) {
 			case 0:
@@ -102,8 +118,8 @@ GLvoid Mouse(int button, int state, int x, int y) {
 		glutTimerFunc(10, Timer, 0);
 	}
 	//squre1 나누기
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && ogx > squre[1][0] && ogx <squre[1][2] && ogy >squre[1][1] && ogy < squre[1][3]) {
-		squreindx = 1;
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && 
+		ogx > squre[1][0] && ogx <squre[1][2] && ogy >squre[1][1] && ogy < squre[1][3]) {
 		for (int i = 0; i < 4; i++) {
 			switch (i) {
 			case 0:
@@ -151,6 +167,188 @@ GLvoid Mouse(int button, int state, int x, int y) {
 		sqr2stop = true;
 		glutTimerFunc(10, Timer2, 0);
 	}
+	//squre2 나누기
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN &&
+		ogx > squre[2][0] && ogx <squre[2][2] && ogy >squre[2][1] && ogy < squre[2][3]) {
+		for (int i = 0; i < 4; i++) {
+			switch (i) {
+			case 0:
+				divsqure3[i][0] = squre[2][0];
+				divsqure3[i][1] = squre[2][1];
+				divsqure3[i][2] = squre[2][2] - rectsize;
+				divsqure3[i][3] = squre[2][3] - rectsize;
+				divsqure3[i][4] = squre[2][4];
+				divsqure3[i][5] = squre[2][5];
+				divsqure3[i][6] = squre[2][6];
+				break;
+			case 1:
+				divsqure3[i][0] = squre[2][0] + rectsize;
+				divsqure3[i][1] = squre[2][1];
+				divsqure3[i][2] = squre[2][2];
+				divsqure3[i][3] = squre[2][3] - rectsize;
+				divsqure3[i][4] = squre[2][4];
+				divsqure3[i][5] = squre[2][5];
+				divsqure3[i][6] = squre[2][6];
+				break;
+			case 2:
+				divsqure3[i][0] = squre[2][0];
+				divsqure3[i][1] = squre[2][1] + rectsize;
+				divsqure3[i][2] = squre[2][2] - rectsize;
+				divsqure3[i][3] = squre[2][3];
+				divsqure3[i][4] = squre[2][4];
+				divsqure3[i][5] = squre[2][5];
+				divsqure3[i][6] = squre[2][6];
+				break;
+			case 3:
+				divsqure3[i][0] = squre[2][0] + rectsize;
+				divsqure3[i][1] = squre[2][1] + rectsize;
+				divsqure3[i][2] = squre[2][2];
+				divsqure3[i][3] = squre[2][3];
+				divsqure3[i][4] = squre[2][4];
+				divsqure3[i][5] = squre[2][5];
+				divsqure3[i][6] = squre[2][6];
+				break;
+			}
+		}
+		squre[2][0] = -12;
+		squre[2][1] = -12;
+		squre[2][2] = -12;
+		squre[2][3] = -12;
+		sqr3stop = true;
+		glutTimerFunc(10, Timer3, 0);
+	}
+	//squre3 나누기
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN &&
+		ogx > squre[3][0] && ogx <squre[3][2] && ogy >squre[3][1] && ogy < squre[3][3]) {
+		for (int i = 0; i < 4; i++) {
+			switch (i) {
+			case 0:
+				divsqure4[i][0] = squre[3][0];
+				divsqure4[i][1] = squre[3][1];
+				divsqure4[i][2] = squre[3][2] - rectsize;
+				divsqure4[i][3] = squre[3][3] - rectsize;
+				divsqure4[i][4] = squre[3][4];
+				divsqure4[i][5] = squre[3][5];
+				divsqure4[i][6] = squre[3][6];
+				break;
+			case 1:
+				divsqure4[i][0] = squre[3][0] + rectsize;
+				divsqure4[i][1] = squre[3][1];
+				divsqure4[i][2] = squre[3][2];
+				divsqure4[i][3] = squre[3][3] - rectsize;
+				divsqure4[i][4] = squre[3][4];
+				divsqure4[i][5] = squre[3][5];
+				divsqure4[i][6] = squre[3][6];
+				break;
+			case 2:
+				divsqure4[i][0] = squre[3][0];
+				divsqure4[i][1] = squre[3][1] + rectsize;
+				divsqure4[i][2] = squre[3][2] - rectsize;
+				divsqure4[i][3] = squre[3][3];
+				divsqure4[i][4] = squre[3][4];
+				divsqure4[i][5] = squre[3][5];
+				divsqure4[i][6] = squre[3][6];
+				break;
+			case 3:
+				divsqure4[i][0] = squre[3][0] + rectsize;
+				divsqure4[i][1] = squre[3][1] + rectsize;
+				divsqure4[i][2] = squre[3][2];
+				divsqure4[i][3] = squre[3][3];
+				divsqure4[i][4] = squre[3][4];
+				divsqure4[i][5] = squre[3][5];
+				divsqure4[i][6] = squre[3][6];
+				break;
+			}
+		}
+		squre[3][0] = -12;
+		squre[3][1] = -12;
+		squre[3][2] = -12;
+		squre[3][3] = -12;
+		sqr4stop = true;
+		glutTimerFunc(10, Timer4, 0);
+	}
+	//squre4 나누기
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN &&
+		ogx > squre[4][0] && ogx <squre[4][2] && ogy >squre[4][1] && ogy < squre[4][3]) {
+		for (int i = 0; i < 4; i++) {
+			switch (i) {
+			case 0:
+				divsqure5[i][0] = squre[4][0];
+				divsqure5[i][1] = squre[4][1];
+				divsqure5[i][2] = squre[4][2] - rectsize;
+				divsqure5[i][3] = squre[4][3] - rectsize;
+				divsqure5[i][4] = squre[4][4];
+				divsqure5[i][5] = squre[4][5];
+				divsqure5[i][6] = squre[4][6];
+
+				divsqure5[i+4][0] = squre[4][0];
+				divsqure5[i+4][1] = squre[4][1];
+				divsqure5[i+4][2] = squre[4][2] - rectsize;
+				divsqure5[i+4][3] = squre[4][3] - rectsize;
+				divsqure5[i+4][4] = squre[4][4];
+				divsqure5[i+4][5] = squre[4][5];
+				divsqure5[i+4][6] = squre[4][6];
+				break;
+			case 1:
+				divsqure5[i][0] = squre[4][0] + rectsize;
+				divsqure5[i][1] = squre[4][1];
+				divsqure5[i][2] = squre[4][2];
+				divsqure5[i][3] = squre[4][3] - rectsize;
+				divsqure5[i][4] = squre[4][4];
+				divsqure5[i][5] = squre[4][5];
+				divsqure5[i][6] = squre[4][6];
+
+				divsqure5[i+4][0] = squre[4][0] + rectsize;
+				divsqure5[i+4][1] = squre[4][1];
+				divsqure5[i+4][2] = squre[4][2];
+				divsqure5[i+4][3] = squre[4][3] - rectsize;
+				divsqure5[i+4][4] = squre[4][4];
+				divsqure5[i+4][5] = squre[4][5];
+				divsqure5[i+4][6] = squre[4][6];
+				break;
+			case 2:
+				divsqure5[i][0] = squre[4][0];
+				divsqure5[i][1] = squre[4][1] + rectsize;
+				divsqure5[i][2] = squre[4][2] - rectsize;
+				divsqure5[i][3] = squre[4][3];
+				divsqure5[i][4] = squre[4][4];
+				divsqure5[i][5] = squre[4][5];
+				divsqure5[i][6] = squre[4][6];
+
+				divsqure5[i+4][0] = squre[4][0];
+				divsqure5[i+4][1] = squre[4][1] + rectsize;
+				divsqure5[i+4][2] = squre[4][2] - rectsize;
+				divsqure5[i+4][3] = squre[4][3];
+				divsqure5[i+4][4] = squre[4][4];
+				divsqure5[i+4][5] = squre[4][5];
+				divsqure5[i+4][6] = squre[4][6];
+				break;
+			case 3:
+				divsqure5[i][0] = squre[4][0] + rectsize;
+				divsqure5[i][1] = squre[4][1] + rectsize;
+				divsqure5[i][2] = squre[4][2];
+				divsqure5[i][3] = squre[4][3];
+				divsqure5[i][4] = squre[4][4];
+				divsqure5[i][5] = squre[4][5];
+				divsqure5[i][6] = squre[4][6];
+
+				divsqure5[i+4][0] = squre[4][0] + rectsize;
+				divsqure5[i+4][1] = squre[4][1] + rectsize;
+				divsqure5[i+4][2] = squre[4][2];
+				divsqure5[i+4][3] = squre[4][3];
+				divsqure5[i+4][4] = squre[4][4];
+				divsqure5[i+4][5] = squre[4][5];
+				divsqure5[i+4][6] = squre[4][6];
+				break;
+			}
+		}
+		squre[4][0] = -12;
+		squre[4][1] = -12;
+		squre[4][2] = -12;
+		squre[4][3] = -12;
+		sqr5stop = true;
+		glutTimerFunc(10, Timer5, 0);
+	}
 }
 //squre0 소멸
 GLvoid Timer(int value) {
@@ -158,11 +356,15 @@ GLvoid Timer(int value) {
 	if (sqr1stop==true) {
 		for (int i = 0; i < 4; i++) {
 			if (divsqure1[i][0] >= divsqure1[i][2]) {
-				divsqure1[i][0] = -10;
-				divsqure1[i][1] = -10;
-				divsqure1[i][2] = -10;
-				divsqure1[i][3] = -10;
+				for (int j = 0; j < 4; j++) {
+					divsqure1[j][0] = -10;
+					divsqure1[j][1] = -10;
+					divsqure1[j][2] = -10;
+					divsqure1[j][3] = -10;
+					sqr1stop = false;
+				}
 				sqr1stop = false;
+				break;
 			}
 			switch (i) {
 			case 0:
@@ -204,11 +406,15 @@ GLvoid Timer2(int value) {
 	if (sqr2stop == true) {
 		for (int i = 0; i < 4; i++) {
 			if (divsqure2[i][0] >= divsqure2[i][2]) {
-				divsqure2[i][0] = -10;
-				divsqure2[i][1] = -10;
-				divsqure2[i][2] = -10;
-				divsqure2[i][3] = -10;
+				for (int j = 0; j < 4; j++) {
+					divsqure2[j][0] = -10;
+					divsqure2[j][1] = -10;
+					divsqure2[j][2] = -10;
+					divsqure2[j][3] = -10;
+					sqr2stop = false;
+				}
 				sqr2stop = false;
+				break;
 			}
 			switch (i) {
 			case 0:
@@ -240,5 +446,156 @@ GLvoid Timer2(int value) {
 		}
 		glutPostRedisplay();
 		glutTimerFunc(10, Timer2, 1);
+	}
+}
+
+//squre2 소멸
+GLvoid Timer3(int value) {
+	float move = 0.0005;
+	if (sqr3stop == true) {
+		for (int i = 0; i < 4; i++) {
+			if (divsqure3[i][0] >= divsqure3[i][2]) {
+				for (int j = 0; j < 4; j++) {
+					divsqure3[j][0] = -10;
+					divsqure3[j][1] = -10;
+					divsqure3[j][2] = -10;
+					divsqure3[j][3] = -10;
+					sqr3stop = false;
+				}
+				sqr3stop = false;
+				break;
+			}
+			switch (i) {
+			case 0:
+				divsqure3[i][0] -= move;
+				divsqure3[i][2] -= move * 2;
+				break;
+			case 1:
+				divsqure3[i][1] -= move;
+				divsqure3[i][3] -= move * 2;
+				break;
+			case 2:
+				divsqure3[i][1] += move * 2;
+				divsqure3[i][3] += move;
+				break;
+			case 3:
+				divsqure3[i][0] += move * 2;
+				divsqure3[i][2] += move;
+				break;
+			}
+		}
+		glutPostRedisplay();
+		glutTimerFunc(10, Timer3, 1);
+	}
+}
+//squre3 소멸
+GLvoid Timer4(int value) {
+	float move = 0.0005;
+	if (sqr4stop == true) {
+		for (int i = 0; i < 4; i++) {
+			if (divsqure4[i][0] >= divsqure4[i][2]) {
+				for (int j = 0; j < 4; j++) {
+					divsqure4[j][0] = -10;
+					divsqure4[j][1] = -10;
+					divsqure4[j][2] = -10;
+					divsqure4[j][3] = -10;
+					sqr4stop = false;
+				}
+				sqr4stop = false;
+				break;
+			}
+			switch (i) {
+			case 0:
+				divsqure4[i][0] -= move;
+				divsqure4[i][2] -= move * 2;
+				break;
+			case 1:
+				divsqure4[i][1] -= move;
+				divsqure4[i][3] -= move * 2;
+				break;
+			case 2:
+				divsqure4[i][1] += move * 2;
+				divsqure4[i][3] += move;
+				break;
+			case 3:
+				divsqure4[i][0] += move * 2;
+				divsqure4[i][2] += move;
+				break;
+			}
+		}
+		glutPostRedisplay();
+		glutTimerFunc(10, Timer4, 1);
+	}
+}
+
+GLvoid Timer5(int value) {
+	float move = 0.0005;
+	if (sqr5stop == true) {
+		for (int i = 0; i < 4; i++) {
+			if (divsqure5[i][0] >= divsqure5[i][2]) {
+				for (int j = 0; j < 4; j++) {
+					divsqure5[j][0] = -10;
+					divsqure5[j][1] = -10;
+					divsqure5[j][2] = -10;
+					divsqure5[j][3] = -10;
+
+					divsqure5[j+4][0] = -10;
+					divsqure5[j+4][1] = -10;
+					divsqure5[j+4][2] = -10;
+					divsqure5[j+4][3] = -10;
+
+					sqr5stop = false;
+				}
+				sqr5stop = false;
+				break;
+			}
+			//if(divsqure5[i][0]-divsqure5[i][0])
+			switch (i) {
+			case 0:
+				divsqure5[i][0] -= move;
+				divsqure5[i][1] -= move;
+				divsqure5[i][2] -= move*2;
+				divsqure5[i][3] -= move*2;
+
+				divsqure5[i+4][0] -= move;
+				divsqure5[i + 4][1] += move;
+				divsqure5[i + 4][2] -= move*2;
+				
+				break;
+			case 1:
+				divsqure5[i][1] -= move;
+				divsqure5[i][2] -= move;
+				divsqure5[i][3] -= move*2;
+				
+				divsqure5[i + 4][0] += move*2;
+				divsqure5[i+4][1] -= move;
+				divsqure5[i + 4][2] += move;
+				divsqure5[i+4][3] -= move*2;
+				break;
+			case 2:
+				divsqure5[i][0] -= move;
+				divsqure5[i][1] += move*2;
+				divsqure5[i][2] -= move*2;
+				divsqure5[i][3] += move;
+
+				divsqure5[i+4][1] += move*2;
+				divsqure5[i+4][3] += move;
+				divsqure5[i + 4][0] += move;
+				break;
+			case 3:
+				divsqure5[i][0] += move*2;
+				divsqure5[i][1] += move*2;
+				divsqure5[i][2] += move;
+				divsqure5[i][3] += move;
+
+				divsqure5[i+4][0] += move*2;
+				divsqure5[i+4][2] += move;
+				divsqure5[i + 4][3] -= move;
+				
+				break;
+			}
+		}
+		glutPostRedisplay();
+		glutTimerFunc(10, Timer5, 1);
 	}
 }
