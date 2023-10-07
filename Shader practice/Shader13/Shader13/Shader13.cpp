5cb4ff0ab4a5b631924e5a6ea053332aceb11097
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-10-07 20:32:46 +0900

    Shader13-1 (초기화면)

---
+++ Shader practice/Shader13/Shader13/Shader13.cpp
@@ -0,0 +1,225 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Header.h"
+
+struct Shape {
+	GLfloat vertex[8][3] = { 
+		{-0.5,-0.5,0.0},{-0.5,0.5,0.0},
+		{-0.5,0.5,0.0},{0.5,0.5,0.0},
+		{0.5,0.5,0.0},{0.5,-0.5,0.0},
+		{0.5,-0.5,0.0},{-0.5,-0.5,0.0}
+	};
+	GLfloat colors[8][3] = {
+	{0.0,1.0,0.0}, {1.0,0.0,0.0},
+	{1.0,0.0,0.0},{1.0,1.0,0.0},
+	{1.0,1.0,0.0}, {0.0,0.0,1.0},
+	{0.0,0.0,1.0}, {0.0,1.0,1.0} 
+	};
+	GLuint vao, vbo[2];
+	GLuint ebo;
+};
+
+unsigned int index[] = {
+		0,1,3,
+		1,2,3
+};
+Shape shape;
+
+
+GLfloat rColor=1, gColor=1, bColor=1;
+
+GLint width, height;
+GLuint shaderProgramID;
+GLuint vertexShader;
+GLuint fragmentShader;
+GLchar* vertexSource, * fragmentSource;
+
+/*OPGL관렴 함수*/
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+
+/*셰이더 관련 함수*/
+void make_vertexShaders();
+void make_fragmentShaders();
+void make_shaderProgram();
+
+/*vao, vbo 관련 함수*/
+void Initvbovao();
+void UpdateBuffer();
+void Draw();
+
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{
+	width = 800;
+	height = 600;
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(width, height);
+
+	glutCreateWindow("Drill10");
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	/*초기화 함수*/
+	make_shaderProgram();
+	Initvbovao();
+
+	/*콜백 함수*/
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb"); // Open file for reading
+	if (!fptr) // Return NULL on failure
+		return NULL;
+	fseek(fptr, 0, SEEK_END); // Seek to the end of the file
+	length = ftell(fptr); // Find out how many bytes into the file we are
+	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator
+	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file
+	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer
+	fclose(fptr); // Close the file
+	buf[length] = 0; // Null terminator
+	return buf; // Return the buffer
+}
+
+
+
+GLvoid drawScene() {
+
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(shaderProgramID);
+	/*vao vbo 자동 업데이트*/
+	UpdateBuffer();
+	/*그리기*/
+	Draw();
+
+	glutSwapBuffers(); //--- 화면에 출력하기
+}
+
+GLvoid Reshape(int w, int h) {
+	glViewport(0, 0, w, h);
+}
+
+void Initvbovao()
+{
+
+	int PosLocation = glGetAttribLocation(shaderProgramID, "in_Position");
+	int ColorLocation = glGetAttribLocation(shaderProgramID, "in_Color");
+	glGenVertexArrays(1, &shape.vao);
+	glGenBuffers(2, shape.vbo);
+
+	glBindVertexArray(shape.vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shape.vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(shape.vertex), shape.vertex, GL_STATIC_DRAW);
+	glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shape.vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(shape.colors), shape.colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+
+	glEnableVertexAttribArray(PosLocation);
+	glEnableVertexAttribArray(ColorLocation);
+	
+}
+
+void UpdateBuffer() {
+
+	int PosLocation = glGetAttribLocation(shaderProgramID, "in_Position");
+	int ColorLocation = glGetAttribLocation(shaderProgramID, "in_Color");
+	glBindVertexArray(shape.vao);
+	glEnableVertexAttribArray(PosLocation);
+	glEnableVertexAttribArray(ColorLocation);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shape.vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(shape.vertex), shape.vertex, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shape.vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(shape.colors), shape.colors, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, shape.ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+}
+
+void Draw()
+{
+	int PosLocation = glGetAttribLocation(shaderProgramID, "in_Position");
+	int ColorLocation = glGetAttribLocation(shaderProgramID, "in_Color");
+
+	glBindVertexArray(shape.vao);
+	glLineWidth(5);
+	
+	glDrawArrays(GL_LINES, 0, 8);
+	glDisableVertexAttribArray(PosLocation);
+	glDisableVertexAttribArray(ColorLocation);
+	
+}
+
+void make_shaderProgram()
+{
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	//-- shader Program
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//--- Shader Program 사용하기
+	glUseProgram(shaderProgramID);
+}
+
+void make_vertexShaders()
+{
+	vertexSource = filetobuf("vertex.glsl");
+	//--- 버텍스 세이더 객체 만들기
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	fragmentSource = filetobuf("fragment.glsl");
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
