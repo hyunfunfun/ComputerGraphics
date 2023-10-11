ce58ab87e4750608e92dcb27348a394b5f34b930
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-10-11 23:21:54 +0900

    3D shader test-1

---
+++ 3D Shader practice/3D Shader test/3D Shader test/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
+
+#include <stdlib.h>
