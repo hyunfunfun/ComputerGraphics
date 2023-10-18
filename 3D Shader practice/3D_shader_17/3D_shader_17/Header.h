78f3863be0416170afc299825f8a0b282e6b809d
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-10-19 00:59:22 +0900

    3D shader 17 -1 이동

---
+++ 3D Shader practice/3D_shader_17/3D_shader_17/Header.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <GL/freeglut_ext.h>
+
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+#include <stdlib.h>
+#include <random>
+#include <fstream>
+#include <iterator>
+#include <conio.h>
+
+using namespace std;
